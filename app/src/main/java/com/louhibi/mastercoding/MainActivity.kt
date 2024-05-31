bbad507806148922678816212f6bcced99e8df8f
Mouad <mouadlouhibi2000@gmail.com>
2024-05-31 10:51:24 +0100

    Operations

--- app/src/main/java/com/louhibi/mastercoding/MainActivity.kt
+++ app/src/main/java/com/louhibi/mastercoding/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     private lateinit var result: TextView
+    private var currentInput = StringBuilder()
+    private var operator: String? = null
+    private var operand1: Double? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,6 +33,45 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onButtonClick(button: Button) {
+        val buttonText = button.text.toString()
+        when (buttonText) {
+            "C" -> {
+                currentInput.clear()
+                operator = null
+                operand1 = null
+                result.text = "0"
+            }
+            "=" -> {
+                val operand2 = currentInput.toString().toDoubleOrNull()
+                if (operand1 != null && operator != null && operand2 != null) {
+                    val resultTXT = calculateResult(operand1!!, operand2, operator!!)
+                    result.text = resultTXT.toString()
+                    currentInput.clear().append(resultTXT)
+                    operator = null
+                    operand1 = null
+                }
+            }
+            "+", "-", "*", "/" -> {
+                if (currentInput.isNotEmpty()) {
+                    operand1 = currentInput.toString().toDoubleOrNull()
+                    operator = buttonText
+                    currentInput.clear()
+                }
+            }
+            else -> {
+                currentInput.append(buttonText)
+                result.text = currentInput.toString()
+            }
+        }
+    }
 
+    private fun calculateResult(operand1: Double, operand2: Double, operator: String): Any {
+        return when (operator) {
+            "+" -> operand1 + operand2
+            "-" -> operand1 - operand2
+            "*" -> operand1 * operand2
+            "/" -> operand1 / operand2
+            else -> 0.0
+        }
     }
 }
