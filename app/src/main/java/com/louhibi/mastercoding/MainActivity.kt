200b6a4a7b289a29bb3d53f2e09e19129f744189
Mouad <mouadlouhibi2000@gmail.com>
2024-05-31 10:32:31 +0100

    View Init

--- app/src/main/java/com/louhibi/mastercoding/MainActivity.kt
+++ app/src/main/java/com/louhibi/mastercoding/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.louhibi.mastercoding
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var result: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        result = findViewById(R.id.Result)
+
+        val buttons = listOf(
+            R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6,
+            R.id.btn7, R.id.btn8, R.id.btn9, R.id.btnadd, R.id.btnclear, R.id.btndivide,
+            R.id.btnmul, R.id.btnequal, R.id.btnsub
+        )
+
+        for (id in buttons) {
+            findViewById<Button>(id).setOnClickListener { onButtonClick(it as Button) }
         }
+    }
+
+    private fun onButtonClick(button: Button) {
+
     }
 }
